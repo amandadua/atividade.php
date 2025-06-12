ab356b2317fe7ee9f5f37df1367061f68c561cb9
amandadua <amandaduartesantos13@outlook.com>
2025-06-12 12:32:56 -0300

    Função Boas-Vindas - último exercício

---
+++ funcaoBoasVindas.php
@@ -0,0 +1,11 @@
+<?php
+
+function boasVindas($nome) {
+    echo "Bem-vindo(a), $nome<br>";
+}
+
+echo boasVindas("Amanda<br>");
+echo boasVindas("Anabelle<br>");
+echo boasVindas("Anderson<br>");
+
+?>
