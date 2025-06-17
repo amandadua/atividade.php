d83722ff2cdd3069188833afd79d38a79879dd1a
amandadua <amandaduartesantos13@outlook.com>
2025-06-17 08:47:48 -0300

    Total de compras

---
+++ totalCompras.php
@@ -0,0 +1,10 @@
+<?php
+
+$produtos = [4, 6, 2, 9, 5];
+$total = 0;
+
+foreach ($produtos as $produtos) {
+    $total += $produtos;
+}
+echo "Seu carrinho de compras tem o valor total de: R$ " . $total;
+?>
